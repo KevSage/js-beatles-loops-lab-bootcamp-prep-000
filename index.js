075b70fb9073a91b9b16702b321849b6477747d0
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-06 01:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+var arr = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+   arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}// add solution here
+var factsArr = [];
+function johnLennonFacts(array) {
+  let i = array.length  - 1;
+  while (i > 0) {
+    factsArr.push(array[i] + "!!!");
+    --i;
+  }
+  return factsArr;
+}
